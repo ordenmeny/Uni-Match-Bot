e216d694c8959d66ab773290104efceb485d80f1
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-02-23 12:44:01 +0500

    Улучшено качество кода, добавлены новые api-запросы

--- app/api_requests.py
+++ app/api_requests.py
@@ -1,3 +1,5 @@
+import logging
+
 import aiohttp
 import os
 from dotenv import load_dotenv
@@ -6,23 +8,44 @@ load_dotenv()
 HOST = os.getenv("HOST")
 DRF_API_TOKEN = os.getenv("DRF_API_TOKEN")
 
+
+async def patch_request(url, data, headers):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.patch(url, json=data, headers=headers) as response:
+                if response.status in (200, 201):
+                    result = await response.json()
+                    return result
+                else:
+                    logging.error(f"Ошибка {response.status}: {await response.text()}")
+                    return None
+        except aiohttp.ClientError as e:
+            logging.error(f"Сетевой сбой: {e}")
+            return None
+
+
 async def set_chat_id(chat_id, uniq_code):
     headers = {"Authorization": f"Token {DRF_API_TOKEN}"}
-
-    url = f"{HOST}/users/api/get-bot/{uniq_code}/"
+    url = f"{HOST}/users/api/get-user-by-uniq-code/"
     data = {
         "uniq_code": "",
         "chat_id": chat_id
     }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.patch(url, json=data, headers=headers) as response:
-            if response.status in (200, 201):
-                result = await response.json()
-                return result
-            else:
-                # error_message = await response.text()
-                return None
+    return await patch_request(url, data, headers)
+
+
+
+async def set_uniq_code_by_chat_id(chat_id, uniq_code):
+    headers = {"Authorization": f"Token {DRF_API_TOKEN}"}
+    url = f"{HOST}/users/api/get-user-by-chat-id/{chat_id}"
+    data = {
+        "uniq_code": uniq_code,
+    }
+
+    return await patch_request(url, data, headers)
+
+
 
 async def get_user(chat_id):
     headers = {"Authorization": f"Token {DRF_API_TOKEN}"}
--- app/handlers.py
+++ app/handlers.py
@@ -16,12 +16,22 @@ router = Router()
 
 @router.message(CommandStart(deep_link=True))
 async def start_handler(message: Message, command: CommandObject):
-    uniq_code = command.args
-    answer = await set_chat_id(str(message.chat.id), str(uniq_code))
-    if answer:
-        await message.answer("Теперь ваш аккаунт привязан к боту.", reply_markup=main_kb)
+    if command.args[0:6] == "login_":
+        uniq_code = command.args[6:]
+        try:
+            answer = await set_uniq_code_by_chat_id(message.chat.id, uniq_code)
+            if answer is None:
+                raise Exception("Не удалось установить уникальный код для чата.")
+        except Exception as e:
+            logging.error(e)
+
     else:
-        await message.answer("Что-то пошло не так.")
+        uniq_code = command.args
+        answer = await set_chat_id(str(message.chat.id), str(uniq_code))
+        if answer:
+            await message.answer("Теперь ваш аккаунт привязан к боту.", reply_markup=main_kb)
+        else:
+            await message.answer("Что-то пошло не так.")
 
 
 @router.message(CommandStart())
